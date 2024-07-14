ff2e1473481476fbd97e48efe43ca51ed9c8e325
thapapath <130227553+thapapath@users.noreply.github.com>
2024-07-14 21:25:14 +0700

    Initial commit with project files

---
+++ app/test/test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Hello World Test', () => {
+  it('should return Hello, World!', (done) => {
+    chai.request('http://localhost:3000')
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).to.equal('Hello, World!');
+        done();
+      });
+  });
+});
